c1252959bbc08a82b0edbfc83b1e10d0622fdf59
Daniel Ledger <u2233909@live.warwick.ac.uk>
2023-04-30 11:57:20 -0400

    Purged unnecessary files

--- destroy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-readonly STACK_NAME="DL-notes-Stack"
-echo "Destroying stack - this may take a few minutes."
-aws cloudformation delete-stack --stack-name "${STACK_NAME}"
-aws cloudformation wait stack-delete-complete --stack-name "${STACK_NAME}"
-echo "Stack deletion finished."
-
